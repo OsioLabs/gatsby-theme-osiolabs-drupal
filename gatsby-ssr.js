b596aead0b46ceaffaa7f63f3cd6afbaf1ef7b04
Joe Shindelar <eojthebrave@gmail.com>
2020-04-13 11:52:09 -0500

    Initialize context providers in gatsby-ssr.js so they're present during gatsby build.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react';
+import TutorialListProvider from './src/hooks/useTutorialList/TutorialListProvider';
+import DrupalOauth from './src/components/drupal-oauth/DrupalOauth';
+import withDrupalOauthProvider from './src/components/drupal-oauth/withDrupalOauthProvider';
+import withDrupalOauthConsumer from './src/components/drupal-oauth/withDrupalOauthConsumer';
+
+// Initialize a new DrupalOauth client which we can use to seed the context
+// provider.
+const drupalOauthClient = DrupalOauth.createFromEnvironment();
+
+const OauthProvider = withDrupalOauthProvider(
+  drupalOauthClient,
+  React.Fragment
+);
+const TutorialListProviderWithOauthConsumer = withDrupalOauthConsumer(
+  TutorialListProvider
+);
+
+// eslint-disable-next-line react/prop-types
+export const wrapRootElement = ({ element }) => (
+  <OauthProvider>
+    <TutorialListProviderWithOauthConsumer>
+      {element}
+    </TutorialListProviderWithOauthConsumer>
+  </OauthProvider>
+);
